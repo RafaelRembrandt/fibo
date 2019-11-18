f7b63b1938f94463f425ceae63aa8acac1c19e78
Rafael Rembrandt <rembrandtx@live.com>
2019-11-18 09:31:47 -0300

    Add files via upload

---
+++ xd2/src/xd2/Fib.java
@@ -0,0 +1,21 @@
+package xd2;
+
+public class Fib {
+
+	public static void main(String[] args) {
+			int n = 9;
+			System.out.println(fibonacci(n));
+	}
+
+	static int fibonacci(int n) {
+		if(n <= 1) {
+			return n;
+		}else {
+			return fibonacci(n-1) + fibonacci(n-2);	
+		
+		
+		
+		
+	}
+	}
+}
